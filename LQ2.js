33d5a0e7fde78dfc84b3cfd0a9267309dc59193e
ricainigoikang14 <inigorica5@gmail.com>
2025-04-10 23:12:07 +0800

    Create LQ2.js

---
+++ LQ2.js
@@ -0,0 +1,8 @@
+// Loop Statement
+
+let breed = prompt("Please enter the dog breed; ");
+let count = prompt("How many times would you like to print the breed?");
+
+for (let i = 0; i< count; i++) {
+    console.log(breed);
+}
